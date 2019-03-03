3c5a854a20116ec823ed89ac0f5db9eed33a5670
Jonathan Gan <jonathang132298@gmail.com>
2019-03-02 22:17:21 -0500

    Initial Commit

---
+++ Ball is Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ball is Life
+//
+//  Created by Jonathan Gan on 3/2/19.
+//  Copyright © 2019 Cryptocurrensea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
